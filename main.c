32e10e635a18df950e1cf75e5555ffae4fb60e8b
johannplz <203719216+johannplz@users.noreply.github.com>
2026-05-07 20:26:21 -0300

    Update main.c
    update 1
    
    troquei o nome de "ampola" pra "seringa nas variáveis e corrigi a fórmula do numero de pulsos

--- main.c
+++ main.c
@@ -11,21 +11,25 @@ void app_main() {
   
   //Código para uma roda de 1 cm de raio:
   //como o motor tem um passo de 1.8 grau, e a roda tem 1 cm de raio => cada passo faz com que a ampola se mova aprox. 0.0314 cm
-  //(pois 1.8 grau = 0.0314 rad, e o raio da roda é 1 [para trocar a roda, apenas multiplicar 0.314 pelo valor do raio em cm])
-  //para calcular o volume final de líquido desejado, é necessário saber o diâmetro da ampola da seringa.
+  //(pois 1.8 grau = 0.0314 rad, e o raio da roda é 10 [para trocar a roda, apenas multiplicar 0.0314 pelo valor do raio em cm])
+  //para calcular o volume final de líquido desejado, é necessário saber o diâmetro da ampola da seringa em cm
 
   
   REG_WRITE(GPIO_OUT_REG, 100000); //HIGH no pino 5 > direção no sentido horario
   
 
 
-  //código feito presumindo uma ampola com 2 cm de diâmetro
+  //#####################################
+  //CONFIGURAÇÕES DO USUÁRIO 
+  uint8_t volume_desejado_ml = 3; //exemplo para 3 ml
+  uint8_t diametro_seringa = 2//diâmetro do corpo/barril da seringa em cm
+  uint8_t raio_da_engr = 1//raio da engrenagem que move o êmbolo
+  //#####################################
+  
 
-  uint8_t ml = 3; //exemplo para 3 ml
-  uint8_t diametro //diâmetro da seringa em ml 
-
-
-  int numero_de_pulsos = 
+  uint8_t area_transv_seringa = 3.1415*((diametro_seringa/2)*(diametro_seringa/2)); //Area do circulo = 3.1415 * r² 
+  
+  uint8_t n_de_pulsos = volume_desejado_ml/(area_transv_seringa*0.0314)*(raio_da_engr); //demonstração da fórmula na documentação
 
   for 
   REG_WRITE(GPIO_OUT_REG, 1000000)
